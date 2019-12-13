510f28026f1d018db4f0e0cf4c25d36c66d19a52
Isabel An <isabel.an@wustl.edu>
2019-12-13 02:23:10 -0600

    update 12/13
    updated home page
    did quiz
    added to nominees

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 //form for trivia quiz
 //credit:code from simplestepscode js quiz tutorial
 
+//if checked
+
 var quizContainer=document.getElementById('quiz');
 var submitButton=document.getElementById('submit');
 var resultsContainer=document.getElementById('results');
@@ -9,20 +11,20 @@ var myQuestions=[
   {
     question: "What year did the Grammy Awards begin?",
     answers: {
-      a:"1968",
-      b:"1959",
-      c:"1976",
-      d:"1955"
+      a:" 1968",
+      b:" 1959",
+      c:" 1976",
+      d:" 1955"
     },
     correctAnswer:"b"
   },
   {
     question: "Which of the following bands is the only band to receive a Grammy Legend Award?",
     answers:{
-      a:"The Beatles",
-      b:"The Bee Gees",
-      c:"The Beach Boys",
-      d:"The Rolling Stones"
+      a:" The Beatles",
+      b:" The Bee Gees",
+      c:" The Beach Boys",
+      d:" The Rolling Stones"
     },
     correctAnswer:"b"
   },
@@ -39,10 +41,10 @@ var myQuestions=[
   {
     question: "Who is the youngest solo artist to win Album of the Year?",
     answers: {
-      a:"Adele",
-      b:"Alanis Morisette",
-      c:"Taylor Swift",
-      d:"Lauryn Hill"
+      a:" Adele",
+      b:" Alanis Morisette",
+      c:" Taylor Swift",
+      d:" Lauryn Hill"
     },
     correctAnswer:"c"
   },
@@ -59,10 +61,10 @@ var myQuestions=[
   {
     question: "Who is the only artist who has won Record, Album, and Song of the Year all in one night on two separate occasions?",
     answers: {
-      a:"Michael Jackson",
-      b:"Paul Simon",
-      c:"Bruno Marcs",
-      d:"Adele"
+      a:" Michael Jackson",
+      b:" Paul Simon",
+      c:" Bruno Marcs",
+      d:" Adele"
     },
     correctAnswer:"d"
   },
@@ -79,35 +81,36 @@ var myQuestions=[
   {
     question: "Which of the following won Record of the Year in 2015?",
     answers: {
-      a:"Can't Feel My Face",
-      b:"Blank Space",
-      c:"Uptown Funk",
-      d:"Thinking Out Loud"
+      a:" Can't Feel My Face",
+      b:" Blank Space",
+      c:" Uptown Funk",
+      d:" Thinking Out Loud"
     },
     correctAnswer:"b"
   },
   {
     question: "Who won Best New Artist in 2011?",
     answers: {
-      a:"Justin Bieber",
-      b:"Esperanza Spalding",
-      c:"Drake",
-      d:"Mumford & Sons"
+      a:" Justin Bieber",
+      b:" Esperanza Spalding",
+      c:" Drake",
+      d:" Mumford & Sons"
     },
     correctAnswer:"b"
   },
   {
     question: "Who hosted the 60th Annual Grammy Awards in 2018?",
     answers: {
-      a:"Jimmy Fallon",
-      b:"Jimmy Kimmel",
-      c:"Ellen DeGeneres",
-      d:"James Corden"
+      a:" Jimmy Fallon",
+      b:" Jimmy Kimmel",
+      c:" Ellen DeGeneres",
+      d:" James Corden"
     },
     correctAnswer:"d"
   }
 ];
 function renderQuiz(questions,quizContainer,resultsContainer,submitButton){
+  document.getElementById('results').className='';
   function renderQuestions(questions,quizContainer){
     var output=[];
     var answers;
@@ -144,7 +147,13 @@ function renderQuiz(questions,quizContainer,resultsContainer,submitButton){
         answerContainers[i].style.color="red";
       }
     }
-    resultsContainer.innerHTML=numCorrect+' out of ' + questions.length;
+    if(numCorrect>=5){
+    resultsContainer.innerHTML="Congratulations! You got " + numCorrect+' questions out of ' + questions.length +" correct."+ " You're ready to take on this year's Grammy's." ;
+  }
+  else{
+    resultsContainer.innerHTML="Oh no! You got " + numCorrect+' questions out of ' + questions.length +" correct."+ " Maybe you should tune into this year's Grammy's and brush up on your trivia.";
+  }
+  document.getElementById('results').className="finished";
   }
   renderQuestions(questions,quizContainer);
   submitButton.onclick=function(){
